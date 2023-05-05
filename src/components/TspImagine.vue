5a1349e3ddff9de42ecfb90af9adbe6736d63fc6
i314q159 <i314q159@outlook.com>
2023-05-05 09:21:07 +0800

    update

--- src/components/TspImagine.vue
+++ src/components/TspImagine.vue
@@ -1,63 +1,69 @@
-<template>
-    <div>
-        <h2>算法可视化</h2>
-        <form @submit.prevent="tsp">
-            <label>gas/greedy/dijkstra</label>
-            <input type="name" v-model="name" required />
-            <br />
-            <label>坐标</label>
-            <input type="cpsStr" v-model="cpsStr" required />
-            <br />
-            <button type="submit">生成</button>
-        </form>
-    </div>
 
-    <img src="http://localhost:31415/img/gas.png" alt="gas.png">
-    <img src="http://localhost:31415/img/greedy.png" alt="greedy.png">
-    <img src="http://localhost:31415/img/dijkstra.png" alt="dijkstra.png">
+<template>
+  <div>
+    <h2>算法可视化</h2>
+    <form @submit.prevent="generateImage">
+      <label>算法类型(gas/greedy/dijkstra)</label>
+      <input type="text" v-model="algorithm" required />
+      <br />
+      <label>坐标(格式为"x1,y1 x2,y2 ...")</label>
+      <input type="text" v-model="coordinates" required />
+      <br />
+      <button type="submit">生成</button>
+    </form>
+    <div v-if="imageUrl">
+      <img :src="imageUrl" alt="algorithm visualization" />
+      <button @click="refreshImage">刷新</button>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    data() {
-        return {
-            name: "",
-            cpsStr: "",
+  data() {
+    return {
+      algorithm: "",
+      coordinates: "",
+      imageUrl: "",
+    };
+  },
+  methods: {
+    async generateImage() {
+      try {
+        const params = {
+          cp: this.coordinates,
         };
+        const options = {
+          params,
+        };
+        const response = await axios.get(
+          `http://127.0.0.1:31415/api/v1/img/${this.algorithm}`,
+          options
+        );
+        this.imageUrl = response.data.path;
+      } catch (error) {
+        console.error(error);
+      }
     },
-    methods: {
-        async register() {
-            try {
-                let cpsArray = this.cpsStr.split(" ")
-                let params = new URLSearchParams();
-                for (let key in cpsArray)
-                    params.append("cp", cpsArray[key]);
-
-                let headersList = {
-                    Accept: "*/*",
-                };
-
-                let reqOptions = {
-                    url: "http://127.0.0.1:31415/api/v1/img/" + this.name,
-                    method: "GET",
-                    headers: headersList,
-                    params: params,
-                };
-
-                let response = await axios.request(reqOptions);
-                console.log(response.data);
-
-                // handle successful registration here
-            } catch (error) {
-                console.error(error);
-                // handle registration error here
-            }
-        },
+    async refreshImage() {
+      try {
+        const params = {
+          cp: this.coordinates,
+        };
+        const options = {
+          params,
+        };
+        const response = await axios.get(
+          `http://127.0.0.1:31415/api/v1/img/${this.algorithm}`,
+          options
+        );
+        this.imageUrl = response.data.path;
+      } catch (error) {
+        console.error(error);
+      }
     },
+  },
 };
 </script>
-
-<style scoped>
-</style>
